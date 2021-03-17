aa60717d938861cbb8479f20e6543c13a43669cc
johnyob <ajo41@cam.ac.uk>
2021-03-17 19:50:23 +0000

    Add exports to Prolog library

---
+++ src/Prolog.hs
@@ -0,0 +1,11 @@
+module Prolog 
+    ( module X
+    ) where
+        
+import Prolog.Syntax as X
+import Prolog.Interpreter as X
+import Prolog.Database as X
+import Prolog.Unification.Substitution as X
+import Prolog.Unification.Unify as X
+
+
